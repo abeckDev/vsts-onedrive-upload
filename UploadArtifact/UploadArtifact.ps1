bc2ab76f8853236565bd0a31de3f894def74b7c7
Alexander Beck <github@cc2224.de>
2017-07-28 16:41:35 +0200

    Edited UploadArtifact.ps1 Added a way to connect to Onedrive with a GUI Client (Work for CLI only in progress)

--- UploadArtifact/UploadArtifact.ps1
+++ UploadArtifact/UploadArtifact.ps1
@@ -1,27 +1,40 @@
 #
 # UploadArtifact.ps1
 #
-[CmdletBinding(DefaultParameterSetName = 'None')]
-param(
-	[string][Parameter(Mandatory=$true)] $variable1, 
-	[string] $variable2
-)
 
-Write-Host "Starting UploadArtifact"
+# Requirements: https://www.powershellgallery.com/packages/OneDrive/1.0.3
+# PowerShell 5.0 or higher 
+# Install-Module -Name OneDrive -Scope CurrentUser
 
-#Run Save-Module -Name VstsTaskSdk -Path .\ for get the Powershell VSTS SDK
-# see https://github.com/Microsoft/vsts-task-lib/tree/master/powershell/Docs
-#Trace-VstsEnteringInvocation $MyInvocation
-
-try {
-Write-Host "variable 1: "$variable1
-Write-Host "variable 2: "$variable2
+# GUI way
+$authentication = Get-ODAuthentication -ClientID "9b072ee0-d954-4eae-bfe6-d9a7e8472776"
+$AuthCode = $authentication.access_token
+Get-ODChildItems -AccessToken $AuthCode -Path "/"
 
 
-} catch {
-
-} finally {
-	#Trace-VstsLeavingInvocation $MyInvocation
-}
-
-Write-Host "Ending UploadArtifact"
+### Sample Code
+#
+#[CmdletBinding(DefaultParameterSetName = 'None')]
+#param(
+#	[string][Parameter(Mandatory=$true)] $variable1, 
+#	[string] $variable2
+#)
+#
+#Write-Host "Starting UploadArtifact"
+#
+##Run Save-Module -Name VstsTaskSdk -Path .\ for get the Powershell VSTS SDK
+## see https://github.com/Microsoft/vsts-task-lib/tree/master/powershell/Docs
+##Trace-VstsEnteringInvocation $MyInvocation
+#
+#try {
+#Write-Host "variable 1: "$variable1
+#Write-Host "variable 2: "$variable2
+#
+#
+#} catch {
+#
+#} finally {
+#	#Trace-VstsLeavingInvocation $MyInvocation
+#}
+#
+#Write-Host "Ending UploadArtifact"
